e57762c7cd5119ec891632e9fcb152ae2807db25
jacob-corletto <112209871+jacob-corletto@users.noreply.github.com>
2023-09-25 20:03:27 +0000

    reads in from input.txt 🎉

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # This is the read me file
 -norwayyy 
+
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import datetime
+import json
+
+file_path = 'input.txt'
+file_content = []
+
+# Open the file in read mode ('r')
+with open(file_path, 'r') as file:
+    # Read the entire file content into a string
+    file_content = file.readlines()
+
 def sorter(u):
   print("will do somthing")
   print("this will do something too")
